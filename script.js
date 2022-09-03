30e0e04eca59c9c5e7112dcea2a5c3020b3ecc9d
Isse Mohamed Abukar <99173622+issemoha@users.noreply.github.com>
2022-09-03 02:08:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("\n\Tap/Click on each color to know its importance");
+
+function blmp(){
+    alert("The boue colour stands for the ever lasting freshness, youth, vigour and vitality of our nation.");
+}
+function cImp(){
+    alert("The white circle denotes the blood-shedding of our national heroes who have sacrificed their lives for our country");
+}
+
